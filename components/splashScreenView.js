901bfdc05d08ce10fa19fe40412b5a7dc01b83ab
Luiz Adriano <santosluiz1912@gmail.com>
2024-09-18 11:50:01 -0300

    Refactor splash screen view to add logo image

--- components/splashScreenView.js
+++ components/splashScreenView.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 export function SplashScreenView() {
   return (
     <View style={styles.container}>
-      <Text>(aqui vai uma logo muito pica da academia)</Text>
+     <Image style={styles.image} source={"https://cdn-icons-png.flaticon.com/512/755/755298.png" ? { uri: "https://cdn-icons-png.flaticon.com/512/755/755298.png" } : null} />
     </View>
   );
 };
@@ -15,4 +15,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  image: {
+    width: 200,
+    height: 200,
+    borderWidth: 4,
+    borderColor: 'black',
+    borderRadius: 10,
+    resizeMode: 'contain',
+  }
 });
